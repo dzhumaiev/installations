54bf3f7139354c027f572fbab4602f39eb56c8ca
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-10-31 09:34:00 +0200

    comments and todo plans

--- init-install.sh
+++ init-install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-# 
+# add comments and sleep
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+# add comments and sleep
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
-#
+# add comments and sleep
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
-#
+# add comments and sleep
 apt update && apt full-upgrade -y
-#
+# add comments and sleep
 apt install pve-kernel-6.2 -y
-#
+# add comments and sleep and input claryfication for reboot
 systemctl reboot
