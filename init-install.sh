934b1eb2b26c91a4de28ecb31b7ba4e010ed43cc
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-10-31 09:12:34 +0200

    debian innit updates

--- init-install.sh
+++ init-install.sh
@@ -3,3 +3,9 @@
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
 #
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+#
+apt update && apt full-upgrade -y
+#
+apt install pve-kernel-6.2 -y
+#
+systemctl reboot
