9ba1576b7de412c05749c5626af03e1918c3cf8a
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-11-01 08:35:26 +0200

    added schos and read expectations

--- add-ssd.sh
+++ add-ssd.sh
@@ -2,25 +2,24 @@
 
 # !!! did not test !!! it need to troubleshoot
 
-# Check if the script is run as root (or with sudo)
+echo "# Check if the script is run as root (or with sudo)" && sleep 5
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or using sudo."
+  echo "# Please run this script as root or using sudo."
   exit 1
 fi
 
-# List available storage devices
+echo "# List available storage devices" && sleep 5
 lsblk
+echo "#  Prompt the user to select an SSD device" && sleep 5
+read -p "# Enter the SSD device (e.g., /dev/sdX): " ssd_device
 
-# Prompt the user to select an SSD device
-read -p "Enter the SSD device (e.g., /dev/sdX): " ssd_device
-
-# Create a GPT partition table on the selected device
+echo "# Create a GPT partition table on the selected device" && sleep 5
 fdisk $ssd_device <<EOF
 g
 w
 EOF
 
-# Create a single partition that spans the entire disk
+echo "# Create a single partition that spans the entire disk" && sleep 5
 fdisk $ssd_device <<EOF
 n
 1
@@ -28,21 +27,30 @@ n
 w
 EOF
 
-# Format the partition as ext4
+echo "# Format the partition as ext4" && sleep 5
 mkfs.ext4 ${ssd_device}1
 
-# Mount the partition temporarily
-mount ${ssd_device}1 /mnt
+read -p "# Enter the mount point ssd02 for device (like /mnt/ssd02): " mount_point
 
-# Generate a UUID for the partition
+echo "# Create mount point $mount_point" && sleep 5
+mkdir $mount_point
+ls $mount_point -lah
+
+echo "# Mount the partition temporarily" && sleep 5
+mount ${ssd_device}1 $mount_point
+lsblk
+
+echo "# Generate a UUID for the partition" && sleep 5
 uuid=$(blkid -o value -s UUID ${ssd_device}1)
 
-# Add an entry to /etc/fstab
-echo "UUID=$uuid /mnt ext4 defaults 0 0" >> /etc/fstab
+echo "# Add an entry to /etc/fstab" && sleep 5
+echo "UUID=$uuid $mount_point ext4 defaults 0 0" >> /etc/fstab
 
-# Mount the partition from /etc/fstab
+echo "# Mount the partition from /etc/fstab" && sleep 5
 mount -a
 
-# Inform the user about the successful setup
-echo "SSD storage device is now formatted with ext4, added to /etc/fstab, and mounted at /mnt."
+echo "# SSD storage device $ssd_device is now formatted with ext4, added to /etc/fstab, and mounted at $mount_point" && sleep 5
+lsblk
+
+exit
 
