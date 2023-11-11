bab8c89f93e61011903b3168f8f3a3cc11b6130a
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-11-11 14:29:21 +0200

    nas adding as separated script

--- 04_add-ssd.sh
+++ 04_add-ssd.sh
@@ -73,9 +73,5 @@ echo "# Add storage to proxmox" && sleep 5
 pvesm add dir SSD02 --path $mount_point
 pvesm status
 
-echo "# Add NAS for backups" && sleep 5
-pvesm add cifs <NAS-account-name> --server <NAS-URL> --share <NAS-account-name> --prune-backups keep-last=2 --content backup --username <username> --password <password>
-pvesm status
-
 exit
 
