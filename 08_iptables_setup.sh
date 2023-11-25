75caa813ca8fcb5824f144afc6d05dd3f8f5f06f
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-11-25 19:44:13 +0200

    seft tests

--- 08_iptables_setup.sh
+++ 08_iptables_setup.sh
@@ -6,10 +6,16 @@ echo "iptables Settings script turned in to the 'Prints a trace of commands' mod
 iptables-save > /etc/iptables_rules
 chmod +x /etc/iptables_rules
 
+echo "# ! Check just created /etc/iptables_rules" && sleep 2
+ls /etc/ -lah | grep iptables_rules
+
+echo "# Create iptables setup directly after NIC up" && sleep 5
 echo "#!/bin/sh" > /etc/network/if-up.d/iptables
 echo "iptables-restore < /etc/iptables_rules" >> /etc/network/if-up.d/iptables
 echo "exit" >> /etc/network/if-up.d/iptables
 chmod +x /etc/network/if-up.d/iptables
+
+echo "# ! Check /etc/network/if-up.d/iptables" && sleep 2
 ls -lah /etc/network/if-up.d
 cat /etc/network/if-up.d/iptables
 
