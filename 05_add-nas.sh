0fdf587ed01990beec62b1df08b182303c001bf1
Ruslan Dzhumaiev <ruslan.dzhumaiev@gmail.com>
2023-11-25 19:18:54 +0200

    add the backup copies keeping

--- 05_add-nas.sh
+++ 05_add-nas.sh
@@ -4,10 +4,11 @@ set -x
 echo "NAS adding script turned in to the 'Prints a trace of commands' mode" && sleep 5
 
 echo "# Add NAS for backups" && sleep 5
-read -p "Enter NAS account name" NAS_account_name
-read -p "Enter NAS account password" NAS_account_pwd
-read -p "Enter NAS URL" NAS_url
-pvesm add cifs $NAS_account_name --server $NAS_url --share $NAS_account_name --prune-backups keep-last=4 --content backup --username $NAS_account_name --password $NAS_account_pwd
+read -p "Enter NAS account name: " NAS_account_name
+read -p "Enter NAS account password: " NAS_account_pwd
+read -p "Enter NAS URL: " NAS_url
+read -p "Enter a number of backup to keep 0 or 1 or 2 or more: " NAS_backupkeeps
+pvesm add cifs $NAS_account_name --server $NAS_url --share $NAS_account_name --prune-backups keep-last=$NAS_backupkeeps --content backup --username $NAS_account_name --password $NAS_account_pwd
 pvesm status
 
 set +x
